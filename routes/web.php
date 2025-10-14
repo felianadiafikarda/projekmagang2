516c742019bfe96e052848c006ac916ab934472f
Vayazabrina <2200018454@webmail.uad.ac.id>
2025-10-15 00:19:15 +0700

    web reviewer

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< vaya
 Route::get('/reviewer', function () {
     return view('reviewer');
+=======
+
+// Halaman utama author (form kirim artikel)
+Route::get('/author/kirim-artikel', function () {
+    return view('author.KirimArtikel'); // pastikan file-nya ada di resources/views/author/kirim-artikel.blade.php
+>>>>>>> main
 });
